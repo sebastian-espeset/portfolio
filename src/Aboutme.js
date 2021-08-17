39b59cc0fa18f82300e44a305079ab998ccba038
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-08-17 14:21:55 -0500

    ready to test deployment

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -2,26 +2,39 @@ import React from "react";
 import { Container, Typography, Box } from "@material-ui/core";
 import useStyles from "./styles";
 
-const blinkingIcon=require("./BLINKINGICON.gif");
+const blinkingIcon = require("./BLINKINGICON.gif");
 
 export default function Aboutme() {
-  const classes=useStyles();
+  const classes = useStyles();
 
   return (
-    <Box className={classes.aboutMe}>
+    <Container className={classes.aboutMe}>
       <Typography variant="h5">
-        Hey there. My name is Sebastian Espeset. I'm a recent graduate from Lambda School's full-stack web dev track.
+        Hello there, my name is Sebastian Espeset.{" "}
       </Typography>
       <br></br>
       <Typography variant="body1">
-        A bit about my front end...
-        React, Redux, Material UI, SASS, HTML5, Cascading Style Sheets
+        <space></space>Before the pandemic, I worked in mental health. I’ve been a caregiver
+        for developmentally disabled adults, and an RA at a substance abuse
+        treatment center. Most of my life has been dedicated to creative stuff
+        in one form or another. This pursuit has led me through some pretty
+        fantastic experiences, but with the dawning of Covid, sharing my work
+        came to a grinding halt. So I was inspired to start a career in tech. I
+        enjoy software engineering. I love building stuff. I love solving
+        puzzles. I love the immediacy of front-end programming, and I love the
+        headiness of back-end development. 
+        <br></br>
+        As I write this, I’m struggling
+        through my first job hunt. The thing keeping me going is learning new
+        stuff. I built my first fullstack app in python the other day using
+        Flask. I am not proficient in programming applications in Python.
+        Completing that application felt like a huge victory, and now I proudly
+        display it on my site. Even though it isn’t very shiny. Mostly I work in
+        React. This site is built on it. I love working in React. Juggling
+        components and state is so satisfying. I’ve used material ui to style,
+        which started as an arbitrary journey into google’s styling library. Now
+        I’ve come to love it.
       </Typography>
-      <Typography variant="body1">
-        A bit about my back end...
-        <img src={blinkingIcon} width="32px"></img>
-        Node js, Knex, Express js, Postgres, Mysql, SQLite
-      </Typography>
-    </Box>
+    </Container>
   );
 }
--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import { Box, Container, Typography, withStyles } from "@material-ui/core";
 import useStyles from "./styles";
 const hrfAsylum = require("./hrf-asylum.png");
@@ -13,7 +13,6 @@ const BlueTextTypography = withStyles({
 // TODO add clickable images which send viewer to hosted site or github repo
 
 // Add responsiveness
-
 
 export default function Projects() {
   const classes = useStyles();
@@ -35,6 +34,80 @@ export default function Projects() {
             </BlueTextTypography>
 
             <Typography variant="body1">
+              HRF-Asylum is a crowd sourced database collection for lawyers
+              representing asylum seekers. Front end is built on react, backend
+              is Node js. Basically, the user uploads a pdf of a case file and
+              the data science magic scrubs those pdfs, redacts private
+              information, and then uploads that pdf to an AWS s3 bucket. The
+              information we chose to keep gets stored in a postgres database.
+              When a user wants to view information, it’s served to them in a
+              graph, or a table. I dove into the back-end first. My team and I
+              were handed a big pile of legacy code. 40% of the code was
+              redundant or unused; routes that led nowhere, models that used
+              iteration rather than sql queries, it was kind of a mess. So my
+              first job was figuring out the data flow from the front end to AWS
+              s3 storage. I had to learn about Okta’s authentication flow, and
+              the authentication process involved in saving files to AWS s3. My
+              team and I ironed out the routes and queries. The front-end commit
+              I had was simple but necessary. In the code we were given, there
+              was an axios in four components that checked local storage for an
+              authentication token every time it was called. I simplified these
+              calls by writing an axiosWithAuth utility function and importing
+              it wherever it was needed.
+            </Typography>
+            <br></br>
+            <Typography variant="subtitle1">
+              React | Context API | Node js | Knex js | AWS | Okta
+            </Typography>
+          </Box>
+        </Container>
+      </Container>
+      {/* <Box shadow={3} marginTop="20px" marginLeft="47px"></Box>
+      <Container className={classes.projectContainer} maxWidth="lg">
+        <Box boxShadow={1} padding="10px">
+          <Container className={classes.projectDescription}>
+            <BlueTextTypography variant="h5" align="left">
+              Another Weather App
+            </BlueTextTypography>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              elementum orci velit, id interdum leo dignissim sed. Aliquam sit
+              amet risus sed mi congue sollicitudin. Integer egestas volutpat
+              libero vitae dignissim. Integer vel mi eleifend, mattis quam sed,
+              viverra est. Integer sed leo congue, scelerisque nibh sed, posuere
+              risus. Donec tempus, nisl ut laoreet posuere, tortor sapien
+              pretium quam, id semper magna augue id diam. Aliquam erat
+              volutpat. Donec bibendum dui a risus interdum sollicitudin. Sed
+              accumsan leo semper, egestas sem ut, mollis odio. Praesent
+              volutpat lobortis metus et blandit. Proin scelerisque ligula est,
+              a malesuada leo auctor at. Vestibulum ante ipsum primis in
+              faucibus orci luctus et ultrices posuere cubilia curae;
+            </Typography>
+            <br></br>
+            <Typography variant="subtitle1">
+              React | Context API | Node js | Knex js | AWS | Okta
+            </Typography>
+          </Container>
+        </Box>
+        <img
+          src={hrfAsylum}
+          alt="HRF asylum login page"
+          className={classes.projectImage}
+        ></img>
+      </Container>
+      <Box shadow={3} marginTop="20px" marginLeft="47px"></Box>
+      <Container className={classes.projectContainer} maxWidth="lg">
+        <img
+          src={hrfAsylum}
+          alt="HRF asylum login page"
+          className={classes.projectImage}
+        ></img>
+        <Container className={classes.projectDescription}>
+          <Box boxShadow={1} padding="10px">
+            <BlueTextTypography variant="h5">
+              To be determined
+            </BlueTextTypography>
+            <Typography variant="body1">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
               elementum orci velit, id interdum leo dignissim sed. Aliquam sit
               amet risus sed mi congue sollicitudin. Integer egestas volutpat
@@ -54,71 +127,7 @@ export default function Projects() {
             </Typography>
           </Box>
         </Container>
-      </Container>
-      <Box shadow={3} marginTop="20px" marginLeft="47px"></Box>
-      <Container className={classes.projectContainer} maxWidth="lg">
-      <Box boxShadow={1} padding="10px">
-        <Container className={classes.projectDescription}>
-          <BlueTextTypography variant="h5" align="left">
-            Another Weather App
-          </BlueTextTypography>
-          <Typography variant="body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-            elementum orci velit, id interdum leo dignissim sed. Aliquam sit
-            amet risus sed mi congue sollicitudin. Integer egestas volutpat
-            libero vitae dignissim. Integer vel mi eleifend, mattis quam sed,
-            viverra est. Integer sed leo congue, scelerisque nibh sed, posuere
-            risus. Donec tempus, nisl ut laoreet posuere, tortor sapien pretium
-            quam, id semper magna augue id diam. Aliquam erat volutpat. Donec
-            bibendum dui a risus interdum sollicitudin. Sed accumsan leo semper,
-            egestas sem ut, mollis odio. Praesent volutpat lobortis metus et
-            blandit. Proin scelerisque ligula est, a malesuada leo auctor at.
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-            posuere cubilia curae;
-          </Typography>
-          <br></br>
-          <Typography variant="subtitle1">
-            React | Context API | Node js | Knex js | AWS | Okta
-          </Typography>
-        </Container>
-        </Box>
-        <img
-          src={hrfAsylum}
-          alt="HRF asylum login page"
-          className={classes.projectImage}
-        ></img>
-      </Container>
-      <Box shadow={3} marginTop="20px" marginLeft="47px"></Box>
-      <Container className={classes.projectContainer} maxWidth="lg">
-        <img
-          src={hrfAsylum}
-          alt="HRF asylum login page"
-          className={classes.projectImage}
-        ></img>
-        <Container className={classes.projectDescription}>
-        <Box boxShadow={1} padding="10px">
-          <BlueTextTypography variant="h5">To be determined</BlueTextTypography>
-          <Typography variant="body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-            elementum orci velit, id interdum leo dignissim sed. Aliquam sit
-            amet risus sed mi congue sollicitudin. Integer egestas volutpat
-            libero vitae dignissim. Integer vel mi eleifend, mattis quam sed,
-            viverra est. Integer sed leo congue, scelerisque nibh sed, posuere
-            risus. Donec tempus, nisl ut laoreet posuere, tortor sapien pretium
-            quam, id semper magna augue id diam. Aliquam erat volutpat. Donec
-            bibendum dui a risus interdum sollicitudin. Sed accumsan leo semper,
-            egestas sem ut, mollis odio. Praesent volutpat lobortis metus et
-            blandit. Proin scelerisque ligula est, a malesuada leo auctor at.
-            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-            posuere cubilia curae;
-          </Typography>
-          <br></br>
-          <Typography variant="subtitle1">
-            React | Context API | Node js | Knex js | AWS | Okta
-          </Typography>
-        </Box>
-        </Container>
-      </Container>
+      </Container> */}
     </>
   );
 }
